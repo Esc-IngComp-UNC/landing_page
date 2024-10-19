6cae4cece34c5da401f1de1388315cd6e57201d3
Matias Salinas <salinasmatiasit@gmail.com>
2024-10-18 21:54:34 -0300

    svg files added to contact

--- web/src/components/Contact.jsx
+++ web/src/components/Contact.jsx
@@ -1,3 +1,8 @@
+import youtube from '/svgs/youtube.svg'
+import instagram from '/svgs/instagram.svg'
+import linkedin from '/svgs/linkedin.svg'
+import twitter from '/svgs/twitterx.svg'
+
 function Contact(){
     //TODO add SVG to each link
     return (
@@ -11,11 +16,23 @@ function Contact(){
             <div className="infoR">
             <p>FCEFYN:</p>
 
-            <p>Location: Av.Velez Sarsfield 1611 Av.Velez Sarsfield 299 , Córdoba, Argentina</p>
+            <div className="socialMedia">
             <p>twitter: https://twitter.com/FCEFyN_UNC</p>
+                <img src={twitter} alt="twitterXLogo" />    
+            </div>
+            <div className="socialMedia">
             <p>Instagram:https://www.instagram.com/fcefyn_unc/</p>
-            <p>youtube: https://www.youtube.com/channel/UCCBcN5DbyBA2C7iZBX4i9nw</p>
+                <img src={instagram} alt="InstagramLogo" />
+            </div>
+            <div className="socialMedia">
             <p>linkedin: https://www.linkedin.com/school/fcefyn</p>
+                <img src={linkedin} alt="linkedinLogo" />
+            </div>
+            <div className="socialMedia">
+            <p>youtube: https://www.youtube.com/channel/UCCBcN5DbyBA2C7iZBX4i9nw</p>
+                <img src={youtube} alt="youtubeLogo" />
+            </div>
+                <p>Location: Av.Velez Sarsfield 1611 Av.Velez Sarsfield 299 , Córdoba, Argentina</p>
             </div>
         </div>
 
