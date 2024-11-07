861fafb68c6488bb041723edb1d4a1e76d13730a
Matias Salinas <salinasmatiasit@gmail.com>
2024-11-07 20:56:13 -0300

    collapsible menu for mobile

--- web/src/components/NavBar.jsx
+++ web/src/components/NavBar.jsx
@@ -10,15 +10,20 @@ function NavBar() {
   return (
     <nav>
       <div className="logo">LOGO</div>
-      <div className="menu-icon" onClick={toggleMenu}>
-        ☰ 
-      </div>
-      {isMobile ? (<motion.div className="secciones" animate={(isOpen && isMobile) ? {height:"13rem",width:'100vw',opacity:1} : {opacity:0,height:"0rem",width:"0rem"}} transition={{duration:.4}}>
-        <Link to='novedades' smooth={true} className="seccionlink">Novedades</Link>
-        <Link to='faq' smooth={true} className="seccionlink">Preguntas Frecuentes</Link>
-        <Link to='aboutUs' smooth={true} className="seccionlink">Sobre Nosotros</Link>
-        <Link to='contacto' smooth={true} className="seccionlink">Contactos</Link>
-      </motion.div> ) : (<motion.div className="secciones">
+
+      {isMobile ? (
+            <div className="menuHamburguesa">
+                <div className="menu-icon"  onClick={()=>setIsOpen(!isOpen)}>
+                ☰
+                </div>
+                <motion.div className="secciones" initial={{opacity:0,width:0,}} animate={(isOpen) ? {display:'flex', height:"200px",width:"100%" ,opacity:1,y:0}:{display:'none',width:0,opacity:0,}} transition={{ease:"easeOut"}}>
+                
+                <Link to='novedades' onClick={()=>setIsOpen(!isOpen)}>Novedades</Link>
+                <Link to='faq'  onClick={()=>setIsOpen(!isOpen)}>Preguntas Frecuentes</Link >
+                <Link to='aboutUs'  onClick={()=>setIsOpen(!isOpen)}>Sobre Nostros</Link >
+                <Link to='contacto'  onClick={()=>setIsOpen(!isOpen)}>Contactos</Link >
+                        </motion.div>
+            </div>)  : (<motion.div className="secciones">
         <Link to='novedades' smooth={true} className="seccionlink">Novedades</Link>
         <Link to='faq' smooth={true} className="seccionlink">Preguntas Frecuentes</Link>
         <Link to='aboutUs' smooth={true} className="seccionlink">Sobre Nosotros</Link>
